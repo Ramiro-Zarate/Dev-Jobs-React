ac74b256f006c0a49f808efb139f412d9c4af50a
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-04 12:52:23 -0300

    refactor: move job listing card styles to css modules

--- dev-jobs-react/src/components/JobListingCard.jsx
+++ dev-jobs-react/src/components/JobListingCard.jsx
@@ -1,12 +1,12 @@
 import { JobCard } from './JobCad'
-
+import styles from './JobListingCard.module.css'
 
 
 export function JobListingCard( {jobs} ){
     return (
             
-            <section className="tarjetasResultado">
-            <div className="job-listing">
+            <section className={styles.tarjetasResultado}>
+            <div className={styles.jobListing}>
                 {jobs.map(job => (
                     <JobCard key={job.id} job={job} />
                 ))}
