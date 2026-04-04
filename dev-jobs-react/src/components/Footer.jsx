79245d567cbfb55c74eb75c73b878ec085125242
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-04 12:40:34 -0300

    refactor: move footer styles to css modules

--- dev-jobs-react/src/components/Footer.jsx
+++ dev-jobs-react/src/components/Footer.jsx
@@ -1,6 +1,8 @@
+import styles from './Footer.modules.css'
+
 export function Footer(){
   return(
-    <footer>
+    <footer className={styles.footer}>
       <small>
         &copy; 2025 DevJobs. Todos los derechos reservados.
       </small>
