6c351685afcdf1113f8d876f2b9006be3734ca24
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-05 19:10:14 -0300

    feat: add hook useid and names to select in search form section

--- dev-jobs-react/src/components/SearchFormSection.jsx
+++ dev-jobs-react/src/components/SearchFormSection.jsx
@@ -1,24 +1,35 @@
+import { useId } from 'react'
 import styles from './SearchFormSection.module.css'
 
 export function SearchFormSection(){
+  const idText = useId()
+  const idTechnology = useId()
+  const idLocation = useId()
+  const idExperienceLevel = useId()
+  
+  const handleSumbit = (event) => {
+    event.preventDefault()
+  }
+
     return (
         <section>
         <h1>Encuentra tu próximo trabajo</h1>
         <p>Explora miles de oportunidades en el sector tecnológico.</p>
         
         <div className={styles.formSearch}>
-          <form role="search">
+          <form onSubmit={handleSumbit} role="search">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
               <path d="M21 21l-6 -6" />
             </svg>
-            <input required type="text" placeholder="Buscar empleos, habilidades o empresas" />
+            <input name={idText} required type='text' placeholder="Buscar empleos, habilidades o empresas" />
+            <button type='sumbit'>Buscar</button>
           </form>
         </div>
 
         <div className={styles.searchFilters}>
-          <select name="technology" id="filter-technology">
+          <select name={idTechnology} id="filter-technology">
             <option value="">Tecnología</option>
             <optgroup label="Tecnologías populares">
               <option value="javascript">JavaScript</option>
@@ -34,7 +45,7 @@ export function SearchFormSection(){
             <option value="php">PHP</option>
           </select>
 
-          <select name="location" id="filter-location">
+          <select name={idLocation} id="filter-location">
             <option value="">Ubicación</option>
             <option value="remoto">Remoto</option>
             <option value="cdmx">Ciudad de México</option>
@@ -43,7 +54,7 @@ export function SearchFormSection(){
             <option value="barcelona">Barcelona</option>
           </select>
 
-          <select name="experience-level" id="filter-experience-level">
+          <select name={idExperienceLevel} id="filter-experience-level">
             <option value="">Nivel de experiencia</option>
             <option value="junior">Junior</option>
             <option value="mid">Mid-level</option>
