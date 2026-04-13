bc9f0b7610ce7cc0ec9cf3ab1596d5983929a761
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-13 09:55:42 -0300

    fix: pagination in search

--- dev-jobs-react/src/pages/Search.jsx
+++ dev-jobs-react/src/pages/Search.jsx
@@ -33,6 +33,10 @@ const useFilters = () => {
         if (filters.location) params.append('location', filters.location)
         if (filters.experienceLevel) params.append('level', filters.experienceLevel)
 
+        const offset = (currentPage - 1) * RESULTS_PER_PAGE
+        params.append('limit', RESULTS_PER_PAGE)
+        params.append('offset', offset)
+
         const queryParams = params.toString()
 
         const response = await fetch(`https://jscamp-api.vercel.app/api/jobs?${queryParams}`)
@@ -50,7 +54,7 @@ const useFilters = () => {
     fetchJobs()
   }, [filters, textToFilter, currentPage])
 
-  const totalPages = Math.ceil(jobs.length / RESULTS_PER_PAGE)
+  const totalPages = Math.ceil(total / RESULTS_PER_PAGE)
   
   const handlePageChange = (page) => {
     setCurrentPage(page)
