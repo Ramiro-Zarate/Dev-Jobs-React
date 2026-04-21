2e9aa53e3c93150edd8bfc4a380e9d3a6e827ab2
Ramiro-Zarate <ramazara13@gmail.com>
2026-04-21 19:09:19 -0300

    reafactor: Add component HomeHeroSection to Home.jsx

--- dev-jobs-react/src/pages/Home.jsx
+++ dev-jobs-react/src/pages/Home.jsx
@@ -1,42 +1,14 @@
 import styles from './Home.module.css'
-import  backgroundImage from '../assets/background.webp'
 import { useRouter } from '../hook/useRouter'
+import { HeroHomeSection } from '../components/HeroHomeSection'
 
 export default function HomePage () {
-    const {navigateTo} = useRouter()
-
-    const handleSearch = (event)=>{
-        event.preventDefault()
-        const formData = new FormData(event.target)
-        const searchTerm = formData.get('search')
-
-        const url = searchTerm 
-        ? `/search?text=${encodeURIComponent(searchTerm)}`
-        : '/search'
-
-        navigateTo(url)
-    }
+    
 
     return (
         <main>
         <title>Dev Jobs - Empleos</title>
-        <section className={styles.heroSection}>
-            <img src={backgroundImage} alt="" className={styles.bgImage}/>
-            <h1>Encuentra el trabajo de tus sueños</h1>
-            <p>Únete a la comunidad mas grande de desarrolladores y encuentra tu próxima oportunidad</p>
-            <form role="search" onSubmit={handleSearch}>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg"width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
-                    <input 
-                        name='search'
-                        required
-                        type="text" 
-                        placeholder="Buscar empleos por título, habilidad o empresa"
-                        />
-                    <button type="submit" >Buscar</button>
-                </div>
-            </form>
-        </section>
+        <HeroHomeSection />
         <section className={styles.featureSection}>
             <header>
             <h2>¿Por qué DevJobs?</h2>
