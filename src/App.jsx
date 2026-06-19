3d76f8813953df6903aa4abb3506a80e1f43b638
Ramiro-Zarate <ramazara13@gmail.com>
2026-06-19 10:11:27 -0300

    fix: job card style

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,23 @@ const EditPage = lazy(() => import('./pages/Edit.jsx'))
 
 function App() {
   return (
-    <>
-    <Header />
-    <Suspense fallback={<div>Cargando...</div>}> {/* Añadir fallback mas estetico */}
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/search' element={<SearchPage />} />
-        <Route path='/search/:jobId' element={<JobDetail/>}/>
-        <Route path='/search/:jobId/edit' element={<EditPage />}/>
-        <Route path='/publish' element={<PublishPage />} />
-        <Route path='*' element={<NotFoundPage />}/>
-      </Routes>
-    </Suspense>
-    <Footer />
-  </>
-)
+    <div className="app-layout">
+      <Header />
+      <div className="app-content">
+        <Suspense fallback={<div>Cargando...</div>}> {/* Añadir fallback mas estetico */}
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/search' element={<SearchPage />} />
+            <Route path='/search/:jobId' element={<JobDetail/>}/>
+            <Route path='/search/:jobId/edit' element={<EditPage />}/>
+            <Route path='/publish' element={<PublishPage />} />
+            <Route path='*' element={<NotFoundPage />}/>
+          </Routes>
+        </Suspense>
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
